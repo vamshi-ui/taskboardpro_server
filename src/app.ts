566e9d0b46889d755386e8258a41644d8ed0fd6f
Vamshi Bandela <vamshibandela777@gmail.com>
2025-02-27 21:44:31 +0530

    implemet usermanagement

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import categoryRote from "./routes/category.route";
 import tagsRoute from "./routes/tag.route";
 import taskRoute from "./routes/task.route";
+import { userRoute } from "./routes/usermanagement.route";
 const app = express();
 
 app.use(cors());
@@ -11,5 +12,6 @@ app.use(express.json());
 app.use("/api/category", categoryRote);
 app.use("/api/tag", tagsRoute);
 app.use("/api/task", taskRoute);
+app.use("/api/auth", userRoute);
 
 export default app;
--- src/routes/usermanagement.route.ts
+++ src/routes/usermanagement.route.ts
@@ -8,9 +8,9 @@ import {
 import { authorize } from "../middlewares/auth";
 const router = express.Router();
 
-router.post("/auth/register", insertUser);
-router.post("/auth/login", login);
-router.get("/auth/userinfo", authorize(["admin", "user"]), getUser);
-router.get("/auth/logout", logout);
+router.post("/register", insertUser);
+router.post("/login", login);
+router.get("/userinfo", authorize(["admin", "user"]), getUser);
+router.get("/logout", logout);
 
 export const userRoute = router;
